f292d90024d12268bbc74c80af7eccf6d3a7469a
John Morgan <jpmorgan@ubuntu.(none)>
2012-12-28 11:12:47 -0800

    Updates to testing program

--- shitty.py
+++ shitty.py
@@ -7,19 +7,32 @@ from scipy import constants
 from particle import *
 from dynamics import *
 
+mass_earth = 5.97219e24 # kg
+r_earth = 6.3781e6 # m
+
 def force(c, p1, p2):
     """Compute the force on particle 1, from particle 2."""
+    # Vector connecting two points
     vec_r = p2.pos - p1.pos
+
+    # Distance between
     r = sqrt(dot(vec_r, vec_r))
+
+    # Magnitude of force between
     f_mag = c * p1.charge * p2.charge / r**2. 
+
+    # Return array 
     return f_mag * vec_r / r
     
 
 def testing_program():
-    x = particle(1., [1., 2., 3.], [0., 0., 0.])
-    y = particle(1., [2., 3., 4.], [0., 0., 0.])
+    # Mass of earth at origin
+    x = particle(mass_earth, [0., 0., 0.], [0., 0., 0.])
+    # Test mass at R_earth
+    y = particle(1., [0., 0., r_earth], [0., 0., 0.])
 
-    print force(constants.G, x, y)
+    # Force of earth (x) on 1 kg (y) should be roughly -9.81 kg m s^-2
+    print force(constants.G, y, x)
 
 
 testing_program()
