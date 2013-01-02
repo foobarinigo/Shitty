d1ca391790f76b58b40228beba14d93ac319fdfe
John Morgan <johnpmorgan3@gmail.com>
2013-01-01 21:44:34 -0800

    Fixed a couple mistakes, tuned the test case to be more interesting.  There's a numerics problem with short distances (i.e. we're dealing with point particles, so when you get too close you get unphysical results) that takes some care to avoid.  Probably going to implement softening or some other solution
    For now, run the test case with python nbody_gravity.py

--- dynamics.py
+++ dynamics.py
@@ -4,14 +4,15 @@
 
 from numpy import array, dot, sqrt
 from scipy import constants
-from particle import *
+from visual import color, sphere
+from vparticle import *
 
 class dynamics:
     """Hold important information about this simulation."""
     def __init__(self, max_iterations):
         self.max_iterations = max_iterations    
         self.particles = []
-        self.time_step = 1.0
+        self.time_step = 60.0 # We're going to draw at 60fps, so 1 frame = 1 sec
         self.min_distance = constants.c * 3.155e7 # light year.  Need to do something smarter later
         self.iteration_counter = 0
 
@@ -19,20 +20,25 @@ class dynamics:
     def add_particle(self, p):
         self.particles.append(p)
 
+
     def force(self, c, p1, p2):
         """Compute the force on particle 1, from particle 2."""
         return array([0., 0., 0.])
+
 
     def solve(self, idx):
         """Update the position and velocity of self.particles[idx]."""
         return
 
+
     def go(self):
         for it in range(self.iteration_counter, self.max_iterations):
 
-            # Loop over all particles (keeping in mind f_ij = -f_ji    
             for i in range(0, len(self.particles)):
+                rate(60)
                 self.solve(i)
+                self.particles[i].draw.pos = self.particles[i].pos
+                self.particles[i].draw.trail.append(pos = self.particles[i].pos)
 
             self.iteration_counter += 1
         
@@ -52,12 +58,12 @@ class dynamics:
         mass_earth = 5.97219e24 # kg
         r_earth = 6.3781e6 # m
 
-        self.__init__(1)
+        self.__init__(2000)
         
         # Mass of earth at origin
-        x = particle(mass_earth, [0., 0., 0.], [0., 0., 0.])
-        # Test mass at R_earth
-        y = particle(1., [0., 0., r_earth], [0., 0., 0.])
+        x = vparticle(mass_earth, [0., 0., 0.], [0., 0., 0.], .5 * r_earth, color.blue)
+        # Test mass at R_earth moving orbital velocity
+        y = vparticle(1., [r_earth, 0., 0.], [0., 0.6 * sqrt(constants.G * mass_earth / r_earth), 0.], .05 * r_earth, color.green)
 
         self.add_particle(x)
         self.add_particle(y)
@@ -66,4 +72,3 @@ class dynamics:
         print "\nSelf Test:"
         print "==========\n"
         self.dump()
-
--- nbody_gravity.py
+++ nbody_gravity.py
@@ -12,7 +12,7 @@ class nbody_gravity(dynamics):
     def force(self, c, p1, p2):
         """Compute the force on particle 1, from particle 2."""
         # Vector connecting two points
-        vec_r = p2.pos - p1.pos
+        vec_r = p1.pos - p2.pos
     
         # Distance between
         r = sqrt(dot(vec_r, vec_r))
@@ -20,21 +20,26 @@ class nbody_gravity(dynamics):
         if( r < self.min_distance ):
             self.min_distance = r
 
-        # Magnitude of force between
-        f_mag = c * p1.charge * p2.charge / r**2. 
+        # Magnitude of force between.  There's no softening here, so shit will get crazy if the particles get too close
+        f_mag = - c * p1.charge * p2.charge / r**2. 
 
         # Return array 
         return f_mag * vec_r / r
         
     def solve(self, idx):
         """Solve for the accelleration on particle p[idx]."""
+        # This is a direct nbody solver, so we loop over all other particles to find the force on particle[idx]
         for j in range(idx + 1, len(self.particles)):
-            f_temp = self.force(constants.G, self.particles[idx], self.particles[j])
-            # For short times, the velocity depends linearly on the accelleration
+            f_temp = self.force(constants.G, self.particles[idx], self.particles[j]) * self.time_step
+            # v(t+dt) = v(t) + a*dt
             self.particles[idx].vel += f_temp / self.particles[idx].charge
             # Newton's 3rd law allows us to update the bottom triangle in the particle-particle matrix simultaneous with the top
             self.particles[j].vel -= f_temp / self.particles[j].charge 
+
+        # We can update particle[idx]'s position safely as the influence on particle[j; j>idx] has already been accounted for above
+        self.particles[idx].pos += self.particles[idx].vel * self.time_step
+
         return
 
 x = nbody_gravity(1)
-x.go()
+x.self_test()
